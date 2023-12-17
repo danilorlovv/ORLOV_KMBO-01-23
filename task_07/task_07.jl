c5e25e3c0a2986a72bc5287c4de627c9fad5bddf
Danil <danilorlovv2003@gmail.com>
2023-12-18 00:56:17 +0300

    fix

--- task_07/task_07.jl
+++ task_07/task_07.jl
@@ -1,11 +1,27 @@
 using HorizonSideRobots
 
-r = Robot("maps/task_07.sit", animate = true)
+robot = Robot("maps/task_07.sit", animate = true)
 
-function task7!(robot)
-    while isborder(robot, Nord)
-        move!(robot, Ost)
+function try_move_n!(robot, side, n)
+    if isborder(robot, side)
+        return false
+    else
+        for _ in range(1, n)
+            move!(robot, side)
+        end
+        return true
     end
 end
 
-task7!(r)
+function task7!(robot)
+    steps = 1
+    sides = [Ost, West]
+    i = 0
+    while isborder(robot, Nord)
+        try_move_n!(robot, sides[i % 2 + 1], steps)
+        i = i % 2 + 1
+        steps += 1
+    end
+end
+
+task7!(robot)
